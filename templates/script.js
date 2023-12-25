3148b659e1d83015d58687a6c43b3be007498616
Ritesh <rsengar7@gmail.com>
2023-12-25 14:33:02 -0800

    Templates

---
+++ templates/script.js
@@ -0,0 +1,74 @@
+function showSignup() {
+    document.getElementById("signupForm").style.display = "block";
+    document.getElementById("loginForm").style.display = "none";
+}
+
+function showLogin() {
+    document.getElementById("signupForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "block";
+}
+
+function submitSignup() {
+    const username = document.getElementById("signupUsername").value;
+    const email = document.getElementById("signupEmail").value;
+    const password = document.getElementById("signupPassword").value;
+    const confirm_password = document.getElementById("signupConfirmPassword").value;
+    const country_code = document.getElementById("signupCountryCode").value;
+    const phone_number = document.getElementById("signupPhoneNumber").value;
+
+    const data = {
+        username: username,
+        email: email,
+        password: password,
+        confirm_password: confirm_password,
+        country_code: country_code,
+        phone_number: phone_number
+    };
+
+    fetch('http://127.0.0.1:8000/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        console.log('Signup Response:', responseData);
+
+        // Handle the response as needed (e.g., display a success message or redirect)
+    })
+    .catch(error => {
+        console.error('Error during signup:', error);
+        // Handle errors (e.g., display an error message to the user)
+    });
+}
+
+function submitLogin() {
+    const username = document.getElementById("loginUsername").value;
+    const password = document.getElementById("loginPassword").value;
+
+    const data = {
+        username: username,
+        password: password
+    };
+
+    fetch('http://127.0.0.1:8000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        console.log('Login Response:', responseData);
+
+        // Handle the response as needed (e.g., display a success message or redirect)
+    })
+    .catch(error => {
+        console.error('Error during login:', error);
+        // Handle errors (e.g., display an error message to the user)
+    });
+}
+
