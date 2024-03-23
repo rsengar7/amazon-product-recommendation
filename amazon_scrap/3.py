4d86dec81ea2b46b859938d45ff97bdaa3aecf09
Ritesh <rsengar7@gmail.com>
2024-03-22 20:07:28 -0400

    Review Download

--- amazon_scrap/3.py
+++ amazon_scrap/3.py
@@ -25,77 +25,84 @@ Review_rating = []
 Review_date = []
 Review = []
 ASIN = []
+star_rating = []
+review_location = []
 
 
 df = pd.read_csv("amazon_urls.csv")
 
-for index, url in enumerate(df['Urls'].tolist()[0:2000]):
+for index, url in enumerate(df['Urls'].tolist()[0:300]):
     print(index, "----", len(df))
     base_url = url.split("/ref")[0].replace('dp', 'product-reviews')
     all_review_part = "/ref=cm_cr_arp_d_paging_btm_next_{}?ie=UTF8&reviewerType=all_reviews&pageNumber={}"
 
-    review_url = base_url + all_review_part
+    stars = {1: 'one_star', 2: 'two_star', 3: 'three_star', 4: 'four_star', 5: 'five_star'}
+    for number, star in stars.items():
+        all_review_part = "/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&filterByStar={}&pageNumber={}"
+        review_url = base_url + all_review_part
 
-    driver.get(review_url)
+        for i in range(1, 11):
+            # url = "https://www.amazon.com/Scopely-Inc-Stumble-Guys/product-reviews/B0C7WJLQQF/ref=cm_cr_arp_d_paging_btm_next_{}?ie=UTF8&reviewerType=all_reviews&pageNumber={}".format(i, i)
+            url = review_url.format(star, i)
+            # print(url)
+            driver.get(url)
 
-    for i in range(1, 11):
-        # url = "https://www.amazon.com/Scopely-Inc-Stumble-Guys/product-reviews/B0C7WJLQQF/ref=cm_cr_arp_d_paging_btm_next_{}?ie=UTF8&reviewerType=all_reviews&pageNumber={}".format(i, i)
-        url = review_url.format(i, i)
-        # print(url)
-        driver.get(url)
+            time.sleep(1)
 
-        time.sleep(1)
+            for j in range(1, 11):
+                ASIN.append(url.split("/")[5])
+                star_rating.append(number)
 
-        for j in range(1, 11):
-            ASIN.append(url.split("/")[5])
+                try:
+                    profile_name_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/div[1]/a/div[2]/span".format(j)
+                    data = driver.find_element(by=By.XPATH, value=profile_name_xpath)
+                    # print("User Name : ",data.text)
+                    Names.append(data.text)
+                except:
+                    Names.append("")
 
-            try:
-                profile_name_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/div[1]/a/div[2]/span".format(j)
-                data = driver.find_element(by=By.XPATH, value=profile_name_xpath)
-                # print("User Name : ",data.text)
-                Names.append(data.text)
-            except:
-                Names.append("")
+                try:
+                    rate_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/div[2]/a".format(j)
+                    data = driver.find_element(by=By.XPATH, value=rate_xpath)
+                    # print("Summarized Review : ",data.text)
+                    Review_rating.append(data.text)
+                except:
+                    Review_rating.append("")
 
-            try:
-                rate_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/div[2]/a".format(j)
-                data = driver.find_element(by=By.XPATH, value=rate_xpath)
-                # print("Summarized Review : ",data.text)
-                Review_rating.append(data.text)
-            except:
-                Review_rating.append("")
+                try:
+                    review_date_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/span".format(j)
+                    data = driver.find_element(by=By.XPATH, value=review_date_xpath)
+                    row = data.text.split("the")[1]
+                    print("Review Date: ",row.split("on")[1].strip())
+                    print("Review Location: ",row.split("on")[0].strip())
+                    Review_date.append(row.split("on")[1].strip())
+                    review_location.append(row.split("on")[0].strip())
+                except:
+                    Review_date.append("")
+                    review_location.append("")
 
-            try:
-                review_date_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/span".format(j)
-                data = driver.find_element(by=By.XPATH, value=review_date_xpath)
-                print("Review Date: ",data.text.split("on")[1].strip())
-                Review_date.append(data.text)
-            except:
-                Review_date.append("")
-
-            try:
-                review_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/div[4]/span".format(j)
-                data = driver.find_element(by=By.XPATH, value=review_xpath)
-                # print("Review : ",data.text)
-                Review.append(data.text)
-            except:
-                Review.append("")
-            # print("*"*100)
-
-        # sys.exit()
-        # break
-    # break
+                try:
+                    review_xpath = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div[5]/div[3]/div/div[{}]/div/div/div[4]/span".format(j)
+                    data = driver.find_element(by=By.XPATH, value=review_xpath)
+                    # print("Review : ",data.text)
+                    Review.append(data.text)
+                except:
+                    Review.append("")
+                # print("*"*100)
 
 dict1 = {
     "ASIN": ASIN,
     "Names": Names,
     "Review_rating": Review_rating,
     "Review_date": Review_date,
+    "Rating": star_rating,
+    "Review Location": review_location,
     "Review": Review
 }
 
 df = pd.DataFrame(dict1)
 
-# print(df.head())
+df['Review'] = df['reviewText'].astype(str)
+df = df.dropna(subset=['Review'])  # Drop rows with missing reviews
 
-df.to_csv("amazon_reviews_2000.csv")
+df.to_csv("amazon_reviews_300.csv")
